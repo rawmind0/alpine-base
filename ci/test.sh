9e18bbd852bc01b3e9cdb961ff1047ba9bde3de6
Raul Sanchez <rawmind@gmail.com>
2017-08-31 22:58:20 +0200

    Added ci integration

--- ci/test.sh
+++ ci/test.sh
@@ -10,7 +10,7 @@ CHECK=$(docker run -t --rm ${DOCKER_TAG}:${TAG} cat /etc/alpine-release )
 rc=$?
 if [ $rc -eq 0 ]; then
 	CHECK_VER=$(echo ${CHECK} | cut -d. -f1,2 )
-	if [ "$CHECK_VER" -eq "$VERSION" ]; then 
+	if [ "$CHECK_VER" == "$VERSION" ]; then 
 		log "${DOCKER_TAG}:${TAG} tested"
 	else
 		log "[Error]: testing ${DOCKER_TAG}:${TAG} got $CHECK_VER expected $VERSION"
