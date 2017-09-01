5729c0f6ce585a4a17c604b18e3c89c3fbb9ecd1
Raul Sanchez <rawmind@gmail.com>
2017-09-01 15:59:28 +0200

    Added ci integration

---
+++ ci/release.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+parent_path=$( cd "$(dirname ${BASH_SOURCE[0]})" ; pwd -P )
+source $parent_path/common.sh
+
+if [ -n "$1" ]; then
+	sourceFile "$1"
+fi
+
+log "Finding ${DOCKER_IMAGE} ..."
+list_image=$(docker images -q ${DOCKER_IMAGE})
+checkError $?
+
+exist_image=$(echo $list_image | wc -l)
+if [ "$exist_image" -ne "1" ]; then
+	log "Loading ${DOCKER_IMAGE}..."
+	docker load -i ${DOCKER_FILE}
+	checkError $?
+fi
+
+log "Pushing ${DOCKER_IMAGE} ..."
+echo docker push ${DOCKER_IMAGE}
+#checkError $?
+
+log "Tagging git repo ${TAG} ..."
+echo git tag ${TAG}
+#checkError $?
+
+log "Pushing git tags ${TAG} ..."
+echo git push origin master --tags
+#checkError $?
+
+log "OK"
+exit 0
